19508fbbb07c3dc2290b61387b614a3f233082e5
Sir Knight <Darklord325@office.biik.ru>
2018-11-24 00:42:41 +0800

    Начал разработку. Добавил проект и базовые интерфейсы.

---
+++ Console/ConsolePresenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DesignPatternLibrary;
+
+namespace Console
+{
+    class ConsolePresenter : IPresenter
+    {
+        public string DeleteData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string InsertData(string field, Data data)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Select Commands
+
+        public string[] SelectAllData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] SelectDataBetween(string field, string val1, string val2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] SelectDataWhere(string field, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ DesignPatternLibrary/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternLibrary
+{
+    public class Data
+    {
+        public Data(string date, string text, int value)
+        {
+            this.Date = date;
+            this.Text = text;
+            this.Value = value;
+        }
+
+        string Id { get; set; }
+        string Date { get; set; }
+        string Text { get; set; }
+        int Value { get; set; }        
+    }
+}
